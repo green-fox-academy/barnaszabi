af8ea9dd85760a36d90b5f6f6d4fc15a4aae443b
Barna Szabolcs <barnaszabi97@gmail.com>
2019-09-12 15:29:42 +0200

    Printing to the terminal window exercises

---
+++ week-01/day-4/src/HelloOthers.java
@@ -0,0 +1,9 @@
+public class HelloOthers
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, Zi");
+        System.out.println("Hello, Omar");
+        System.out.println("Hello, Gergő");
+    }
+}
