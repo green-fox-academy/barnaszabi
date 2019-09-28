88078b55f75f5dee29f2cd4e68ddb16831b8a8b8
Barna Szabolcs <barnaszabi97@gmail.com>
2019-09-28 12:26:51 +0200

    Renaming directories

--- week-03/day-02/src/blogpost/Main.java
+++
@@ -1,9 +0,0 @@
-package blogpost;
-
-public class Main {
-  public static void main(String[] args) {
-    BlogPost first = new BlogPost("John Doe","Lorem Ipsum", "Lorem ipsum dolor sit amet.", "2000.05.04.");
-    BlogPost second = new BlogPost("Tim Urban","Wait but why", "A popular long-form, stick-figure-illustrated blog about almost everything.", "2010.10.10.");
-    BlogPost third = new BlogPost("William Turton","One Engineer Is Trying to Get IBM to Reckon With Trump", "Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing.", "2017.03.28.");
-  }
-}
--- week-03/day-02/src/petrolstation/Station.java
+++
@@ -1,9 +0,0 @@
-package petrolstation;
-
-public class Station {
-  int gasAmount;
-  public void refill(Car car){
-    gasAmount -= car.capacity;
-    car.gasAmount += car.capacity;
-  }
-}
