0f6a3f54a014dc03031c7634a49dca5c3ae658cd
Barna Szabolcs <barnaszabi97@gmail.com>
2019-10-22 23:04:38 +0200

    Finished Say Hello to all the world (Web App) exercise

--- week-06/day-02/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWorldWebController.java
+++ week-06/day-02/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWorldWebController.java
@@ -5,12 +5,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Arrays;
+import java.util.Random;
+
 @Controller
 public class HelloWorldWebController {
   @RequestMapping(value = "/web/helloka")
   public String helloInLotOfLanguages(@RequestParam String color, @RequestParam String size, Model model){
+    Random random = new Random();
+    String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+        "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+        "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+        "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
     model.addAttribute("color", "color: " + color);
     model.addAttribute("size", "font-size: " + size);
+    model.addAttribute("text", hellos[random.nextInt(hellos.length)]);
     return "greeting2";
   }
 }
